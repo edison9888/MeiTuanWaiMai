8d566ad9b58356aa2273e265444c4c428685c82d
Sam Lau <liuyaozhu13hao@163.com>
2015-07-15 09:35:53 +0800

    Create project

---
+++ MeiTuanWaiMai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeiTuanWaiMai
+//
+//  Created by Sam Lau on 7/15/15.
+//  Copyright (c) 2015 Sam Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeiTuanWaiMai/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeiTuanWaiMai
+//
+//  Created by Sam Lau on 7/15/15.
+//  Copyright (c) 2015 Sam Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
